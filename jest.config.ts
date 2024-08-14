2f4a41a48fedfab7e69bf8d252b170c6f145bc3a
Svapnil <svapnila@gmail.com>
2024-08-14 10:44:56 -0400

    ignore dist in jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
